ed5a66c1b1fffa3dab04c24b49f906bfb2bd729f
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-02-22 21:04:28 +0530

    Update reverse each word in str.c

--- reverse each word in str.c
+++ reverse each word in str.c
@@ -7,8 +7,6 @@ int main()
 	scanf("%[^\t\n]s",a);
 	n=strlen(a);
 	i=0,j=0;
-	if(n%2!=0)
-	{
 	for(i=0;i<n;i++)
 	{
 	    if(a[i]!=' ')
@@ -31,32 +29,6 @@ int main()
 	for(i=(count-1);i>=0;i--)
 	{
 	    printf("%c",str[i]);
-	}
-	}
-	else
-	{
-	   for(i=0;i<n;i++)
-	    {
-	    if(a[i]!=' ')
-	    {
-	        str[j]=a[i];
-	        j++;
-	        count++;
-	    }
-	    else
-	    {
-	        for(k=(count-1);k>=0;k--)
-	        {
-	            printf("%c",str[k]);
-	        }
-	        j=0;
-	        count=0;
-	    }
-	    }
-	    for(i=(count-1);i>=0;i--)
-	    {
-	        printf("%c",str[i]);
-	    }
 	}
 	return 0;
 }
