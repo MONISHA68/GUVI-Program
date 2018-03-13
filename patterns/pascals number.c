29eeb0edfb0bef51181a40845d61d722b71831bc
MONISHA68 <33077811+MONISHA68@users.noreply.github.com>
2018-03-13 19:56:10 +0530

    Create pascals number.c

---
+++ patterns/pascals number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,k,m=1,n1;
+    scanf("%d ",&n1);
+    for(i=0;i<n1;i++)
+    {
+        for(j=1;j<n1-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=0;k<=i;k++)
+        {
+            printf("%d ",m);
+            m++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
